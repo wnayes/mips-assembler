15bea62d3e384330ff79e6d53c30ff9e4288deda
Will Nayes <wnayes@gmail.com>
2020-12-26 23:48:23 -0600

    More equ tests

--- test/directives/equ.spec.ts
+++ test/directives/equ.spec.ts
@@ -77,4 +77,73 @@ describe("equ", () => {
     expect(dataView.getUint32(0)).to.equal(0x01020304);
     expect(dataView.getUint32(4)).to.equal(0x05060708);
   });
+
+  it("can be used within instructions", () => {
+    const buffer = new ArrayBuffer(12);
+    const dataView = new DataView(buffer);
+    assemble(`
+      StringPointer equ 0x20(SP)
+
+      lw  a0,StringPointer
+      nop
+      sw  a1,StringPointer
+    `, { buffer });
+
+    expect(dataView.getUint32(0)).to.equal(0x8FA40020);
+    expect(dataView.getUint32(4)).to.equal(0x00000000);
+    expect(dataView.getUint32(8)).to.equal(0xAFA50020);
+  });
+
+  it("will recursively process, assuming no infinite recursion", () => {
+    const buffer = new ArrayBuffer(8);
+    const dataView = new DataView(buffer);
+    assemble(`
+      ListA equ 1,2,3,4
+      ListB equ 5,6,7,8
+      List equ ListA,ListB
+      .byte List
+    `, { buffer });
+
+    expect(dataView.getUint32(0)).to.equal(0x01020304);
+    expect(dataView.getUint32(4)).to.equal(0x05060708);
+  });
+
+  it("can handle spaces in equivalence value", () => {
+    const buffer = new ArrayBuffer(8);
+    const dataView = new DataView(buffer);
+    assemble(`
+      ListA equ 1, 2, 3, 4
+      ListB equ 5 6 7 8
+      List equ ListA ListB
+      .byte List
+    `, { buffer });
+
+    expect(dataView.getUint32(0)).to.equal(0x01020304);
+    expect(dataView.getUint32(4)).to.equal(0x05060708);
+  });
+
+  it("can handle quoted string values as equivalence value", () => {
+    const buffer = new ArrayBuffer(8);
+    const dataView = new DataView(buffer);
+    assemble(`
+      text equ "abcdefgh"
+      .ascii text
+    `, { buffer });
+
+    expect(dataView.getUint32(0)).to.equal(0x61626364); // "abcd"
+    expect(dataView.getUint32(4)).to.equal(0x65666768); // "efgh"
+  });
+
+  it("can handle quoted string values as equivalence value (2)", () => {
+    const buffer = new ArrayBuffer(8);
+    const dataView = new DataView(buffer);
+    assemble(`
+      first equ "abcd"
+      second equ 'efgh'
+      .ascii first,second
+    `, { buffer });
+
+    expect(dataView.getUint32(0)).to.equal(0x61626364); // "abcd"
+    expect(dataView.getUint32(4)).to.equal(0x65666768); // "efgh"
+  });
 });
